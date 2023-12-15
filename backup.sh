f0e3ac532f40abbca1b869339ee40fe789654cb7
knbob <email2nagababu@gmail.com>
2023-12-15 20:38:07 +0530

    assignment

--- backup.sh
+++ backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Please enter the source directory to create back: "
-read SD
-echo "Please enter the destination directory to save .tar.gz file: "
-read DD
-echo "Please enter a file name to save .tar.gz file: "
-read fname
-TIME=$(date +%F_%H_%M_%S)
+echo "Please enter the source directory to create back: "               #print message
+read SD                                                                 #Read input from user                                      
+echo "Please enter the destination directory to save .tar.gz file: "    #print message
+read DD                                                                 #Read input from user
+#echo "Please enter a file name to save .tar.gz file: "                  #print message
+#read fname                                                              #Read input from user
+TIME=$(date +%F_%H_%M_%S)                                               #Time stamp
 
 
-folder=$(echo "$SD"| awk -F / '{print $NF}')
-gzfile="$fname-$TIME.tar.gz"
+folder=$(echo "$SD"| awk -F / '{print $NF}')                            # to get folder name from absolute path
+gzfile="$folder-$TIME.tar.gz"                                            
 cd $SD
 cd ..
-tar -r -f "$fname-$TIME.tar.gz" $folder
+tar -r -f "$folder-$TIME.tar.gz" $folder                                 # create .tat.gz file
 mv $gzfile $DD
