6da5cc0eedb6dbe5fa7315cc9446fd77630b1d28
knbob <email2nagababu@gmail.com>
2023-12-15 20:23:23 +0530

    assignment

--- backup.sh
+++ backup.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-echo "Please enter the source directory: "
+echo "Please enter the source directory to create back: "
 read SD
-echo "Please enter the destination directory: "
+echo "Please enter the destination directory to save .tar.gz file: "
 read DD
-echo "Please enter a file name: "
+echo "Please enter a file name to save .tar.gz file: "
 read fname
 TIME=$(date +%F_%H_%M_%S)
-echo "$TIME"
-cd $SD
+
 
 folder=$(echo "$SD"| awk -F / '{print $NF}')
-echo "$folder"
-
-echo "$SD"
-echo "$DD"
-
 cd $SD
 cd ..
 tar -r -f "$fname-$TIME.tar.gz" $folder
